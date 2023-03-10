ee3d28a1d373a9d774831561e50dbf832f4c8958
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2023-03-10 12:06:34 +0200

    cors-uuid-expres

--- index.js
+++ index.js
@@ -5,10 +5,30 @@ const fs = require('fs').promises;
 
 const app = express();
 
-const port = 3001;
+app.use(cors());
 
-app.get
+app.use((req, res, next) => {
+  req.time = new Date().toLocaleString('ua-UA');
+
+  next();
+});
+
+const port = 3000;
+
+// CRUD - create, read, update, delete
+app.get('/users', (req, res) => {
+  // console.log(req);
+
+  // res.send('<h1>Hell world!!!</h1>');
+  res.status(200).json({
+    user: {
+      name: 'Joe',
+      year: 2002,
+      time: req.time,
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server up and running on port: ${port}`);
+  console.log(`Server up and running on port: ${port}`);
 });
